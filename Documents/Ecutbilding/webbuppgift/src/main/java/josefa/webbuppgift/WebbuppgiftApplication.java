371ae8f5486583d49852df9a2da45412c30ce53f
josef <yousef.aho@gmail.com>
2025-01-17 12:50:26 +0100

    Update .gitignore

---
+++ Documents/Ecutbilding/webbuppgift/src/main/java/josefa/webbuppgift/WebbuppgiftApplication.java
@@ -0,0 +1,13 @@
+package josefa.webbuppgift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbuppgiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbuppgiftApplication.class, args);
+	}
+
+}
---
+++ Documents/Ecutbilding/webbuppgift/src/test/java/josefa/webbuppgift/WebbuppgiftApplicationTests.java
@@ -0,0 +1,13 @@
+package josefa.webbuppgift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbuppgiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
